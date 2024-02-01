f620416198e2036bf5252902a25dd076f70ed9cf
Lora Lee B <LoraLee.Barreto@gmail.com>
2024-02-01 16:03:08 -0500

    Tested, corrected indentation issue.

--- 20_digitalstream.py
+++ 20_digitalstream.py
@@ -1,18 +1,18 @@
 """Digital Stream, by Al Sweigart al@inventwith python.com
 A screensaver in the style of The Matrix movie's visuals.
-Vieew this code at https:/nostarch.com/big-book-small-python-projects
+View this code at https:/nostarch.com/big-book-small-python-projects
 Tags: tiny, artistic, beginner, scrolling"""
 
 import random, shutil, sys, time
 
 # Set up the constants:
-MIN_STREAM_LENGTH = 6 # (!) Try changing this to 1 or 50.
+MIN_STREAM_LENGTH =  7 # (!) Try changing this to 1 or 50.
 MAX_STREAM_LENGTH = 14 # (!) Try changing this to 100.
 PAUSE = 0.1 # (!) Try changing this to 0.0 or 2.0.
-STREAM_CHARS = ['LL', 'B'] # (!) Try changing this to other characters.
+STREAM_CHARS = ['L', 'B'] # (!) Try changing this to other characters.
 
 # Density can range from 0.0 to 1.0:
-DENSITY = 0.1 # (!) Try changing this to 0.10 or 0.30.
+DENSITY = 0.05 # (!) Try changing this to 0.10 or 0.30.
 
 # Get the size of the terminal window:
 WIDTH = shutil.get_terminal_size()[0]
@@ -20,9 +20,9 @@ WIDTH = shutil.get_terminal_size()[0]
 # new line automatically, so reduce the width by one:
 WIDTH -= 1
 
-print('Digital Steam, by Al Sweigart al@inventwithpython.com')
+print('Digital Stream, by Al Sweigart al@inventwithpython.com')
 print('Press Ctrl-C to quit.')
-time.sleep(1.5)
+time.sleep(2)
 
 try:
     # For each column, when the counter is 0, no stream is shown.
@@ -37,15 +37,15 @@ try:
                     # Restart a stream on this column.
                     columns[i] = random.randint(MIN_STREAM_LENGTH,
                                                 MAX_STREAM_LENGTH)
-                
-                # Display an empty space or a 1/0 character.
-                if columns[i] > 0:
-                    print(random.choice(STREAM_CHARS), end='')
-                    columns[i] -= 1
-                else:
-                    print(' ', end='')
-            print() # Print a newline at the end of the row of columns.
-            sys.stdout.flush() # Make sure text appears on the screen.
-            time.sleep(PAUSE)
+        
+            # Display an empty space or a 1/0 character.
+            if columns[i] > 0:
+                print(random.choice(STREAM_CHARS), end='')
+                columns[i] -= 1
+            else:
+                print(' ', end='')
+        print() # Print a newline at the end of the row of columns.
+        sys.stdout.flush() # Make sure text appears on the screen.
+        time.sleep(PAUSE)
 except KeyboardInterrupt:
     sys.exit() # When Ctrl-C is pressed, end the program.
